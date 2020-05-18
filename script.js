20f0a2d754da767e5d12eca8de221b0386020f96
phanendraguptha <www.phani122@gmail.com>
2020-05-18 13:24:28 +0530

    addTaskToUi method

--- script.js
+++ script.js
@@ -1,36 +1,63 @@
+window.onload = fetchData;
 window.onkeyup = keyup;
-window.onload = scrollChecker;
-function keyup(e){
-  if(e.keyCode == 13){
+
+var arr = [
+  { task: "wash your hands", completed: false },
+  { task: "check out new todo tutorial", completed: false },
+  { task: "wash your hands", completed: false },
+];
+
+function fetchData() {
+  arr.forEach((item) => {
+    addTasksToUi(item.task);
+  });
+}
+
+var count = 1;
+
+const addTasksToUi = (item) => {
+  var ul = document.getElementById("dynamic-list");
+  var li = document.createElement("li");
+  li.setAttribute("class", "list-item");
+  li.setAttribute("value", `${item}`);
+  var input = document.createElement("input");
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("id", `${count}`);
+  var label = document.createElement("label");
+  label.setAttribute("for", `${count}`);
+  var span = document.createElement("span");
+  span.appendChild(document.createTextNode(item));
+  var img = document.createElement("img");
+  img.setAttribute("src", "./assets/bin.svg");
+  var img1 = document.createElement("img");
+  img1.setAttribute("src", "./assets/edit.svg");
+  li.appendChild(input);
+  li.appendChild(label);
+  li.appendChild(span);
+  li.appendChild(img);
+  li.appendChild(img1);
+  // li.appendChild(document.createTextNode(item));
+  ul.appendChild(li);
+
+  count++;
+  scrollChecker();
+};
+
+function keyup(e) {
+  if (e.keyCode == 13) {
     var input = document.getElementById("input").value;
-
-    var ul = document.getElementById("dynamic-list");
-    var li = document.createElement("li");
-    li.setAttribute('class','list-item');
-    li.appendChild(document.createTextNode(input));
-    ul.appendChild(li);
-
-    var t = scrollChecker();
-    console.log(t);
+    addTasksToUi(input);
   }
 }
 
-function scrollChecker(){
+function scrollChecker() {
   var container = document.querySelector(".container");
   var li = container.getElementsByTagName("li");
-  if(li.length>3){
+  if (li.length > 3) {
     container.setAttribute("style", "overflow-y:scroll");
-    return li.length;
-  }
-  else{
+  } else {
     container.setAttribute("style", "height: 250px;");
   }
 }
 
-function strike(event) {
-  var target = event.target || event.srcElement;
-  target.setAttribute("style", "text-decoration: line-through;")
-  // var ul = document.getElementById("dynamic-list");
-  // ul.removeChild(target);
-}
-
+// const task = `<li> <input type="checkbox" id=${count}> <label for=${count}></label> <span>${item}</span> <img src="./assets/bin.svg" alt="bin"> <img src="./assets/edit.svg" alt="edit"></li>`;
